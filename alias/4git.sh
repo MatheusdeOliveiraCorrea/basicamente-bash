e54db0e13bee51286d3e114e9080b9f626457964
matheus <matheus@pop-os.localdomain>
2023-02-04 17:15:26 -0300

    first commit

---
+++ alias/4git.sh
@@ -0,0 +1,4 @@
+# Definir alias globais para o git
+git config --global alias.st status
+
+echo "git alias definidos..."
---
+++ alias/4postgres.sh
@@ -0,0 +1,4 @@
+# Mudar para usuário 'postgre' e digitar 'psql' para entrar no terminal
+# interativo do PostgreSQL
+
+sudo su - postgres -c "psql"
