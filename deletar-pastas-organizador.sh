15b0fd390236071b97d2c361e1939434207e8ce8
MatheusdeOliveiraCorrea <matheus82pessoal@protonmail.com>
2023-04-21 20:30:24 -0300

    organizando pasta download

---
+++ deletar-pastas-organizador.sh
@@ -0,0 +1,8 @@
+cd ~/Downloads &&
+
+rm -f -r fotos-organizador &&
+rm -f -r pdfs-organizador && 
+rm -f -r videos-organizador && 
+rm -f -r compactados-organizador && 
+rm -f -r arquivos-word-organizador &&
+rm -f -r arquivos-excel-organizador
---
+++ organizar-downloads-folder.sh
@@ -0,0 +1,16 @@
+cd ~/Downloads && 
+
+
+mkdir fotos-organizador && 
+mkdir videos-organizador && 
+mkdir compactados-organizador && 
+mkdir pdfs-organizador && 
+mkdir arquivos-excel-organizador &&
+mkdir arquivos-word-organizador &&
+
+
+mv *.png *.jpeg *.jpg fotos-organizador && 
+mv *.zip *.rar *.tar.gz compactados-organizador && 
+mv *.mp4 videos-organizador && 
+mv *.docx arquivos-word-organizador
+
